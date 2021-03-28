3abaa1ed3c1203604b2f24da90ccb2fc2f9714be
mzyousaf <mzainyousaf.01@gmail.com>
2021-03-28 21:25:24 +0500

    Integration with frontend

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,96 +1,96 @@
-import React, {useState, useContext, useEffect} from 'react';
-import {NavLink as Link, useHistory} from 'react-router-dom';
+import React, { useState, useContext, useEffect } from 'react';
+import { NavLink as Link, useHistory } from 'react-router-dom';
 import ApiClient from '../CommonMethods/APIHandle';
-import {ConfidentialContext} from '../Context/Context';
+import { ConfidentialContext } from '../Context/Context';
 import './SignIn.css';
 
 function SignIn() {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 	const [error, setError] = useState(false);
-	const {value, setValue} = useContext(ConfidentialContext);
+	const { value, setValue } = useContext(ConfidentialContext);
+	let data = { userId: null, token: null, rank: null }
 	let history = useHistory();
 
 	const checkSignIn = (e) => {
-	     e.preventDefault();
-		
-		if(!username || !password){
-			 setError(true)
+		e.preventDefault();
+
+		if (!username || !password) {
+			setError(true)
 		}
-		else{
+		else {
 			setError(false)
 
 			// Sign In API
-			const credentials = {email: username, password: password}
+			const credentials = { email: username, password: password }
 			const apiUrl = 'http://localhost:3090/user/signin/';
 			const customerDataUrl = 'http://localhost:3090/user/';
-		
-			ApiClient.sendPostAuthentication(apiUrl,credentials,(res)=>{
-				if(res.data){
-					console.log(res)
-					setValue({...value, userId: res.data.userId, token: res.data.token})	
-					ApiClient.sendGetAuthentication(customerDataUrl, res.data.userId,(res)=>{
-						if(res){
+
+			ApiClient.sendPostAuthentication(apiUrl, credentials, (res) => {
+				if (res.data) {
+					console.log(res, "Response")
+					data = { ...data, userId: res.data.userId }
+					data = { ...data, token: res.data.token }
+					ApiClient.sendGetAuthentication(customerDataUrl, res.data.userId, (res) => {
+						if (res) {
 							console.log('Data of specific user', res)
-							// setValue({...value, userRank: res.data.rank})
+							data = { ...data, userRank: res.data.rank }
+							setValue({ ...value, userId: data.userId, token: data.token, userRank: data.userRank })
+							console.log(value, ".....data")
 							history.push('/group')
 						}
-						else{
+						else {
 							setError(true)
 							console.log(res)
-						}	
+						}
 					})
-				
+
 				}
-				else{
+				else {
 					setError(true)
 					console.log(res)
-				}	
+				}
 			})
 
 		}
 	}
 
-	useEffect(()=>{
-		console.log('AUTH CONTEXT', value);
-	},[value])
-
-    return (
-        <div className='sing-margin'>
+	return (
+		<div className='sing-margin'>
 			<Link className="sing-create" to="/risk">
 				Create Account
 			</Link>
 
 
-            <div className='signin-title'>
-                <p>Sign In</p>
-            </div>
-            <form className='sign-form'>
+			<div className='signin-title'>
+				<p>Sign In</p>
+			</div>
+			<form className='sign-form'>
 
-                <label style={{color: 'dodgerblue'}}>Email/username</label>
-                <input placeholder={'Please write user name'}
-						type={'text'}
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						autoComplete="off" />
+				<label style={{ color: 'dodgerblue' }}>Email/username</label>
+				<input placeholder={'Please write user name'}
+					type={'text'}
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					autoComplete="off" />
 
-                <label style={{color: 'dodgerblue'}}>Password</label>
-                <input placeholder={'Please write password'}
-						type={'password'}
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						autoComplete="off" />
+				<label style={{ color: 'dodgerblue' }}>Password</label>
+				<input placeholder={'Please write password'}
+					type={'password'}
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					autoComplete="off" />
 
-                <button onClick={(e)=>checkSignIn(e)} >Sign In</button>
-            </form>
+				<button onClick={(e) => checkSignIn(e)} >Sign In</button>
+			</form>
 
 			{error &&
-			<div className="error-credential">
-			   Please write correct credential
+				<div className="error-credential">
+					Please write correct credential
 			</div>
 			}
-        </div>
-    )
+		</div>
+	)
 }
 
 export default SignIn
--- src/Pages/CreateGroup.js
+++ src/Pages/CreateGroup.js
@@ -1,61 +1,68 @@
-import React,{useEffect, useContext, useState} from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import './JoinGroup.css';
-import {ConfidentialContext} from '../Context/Context';
+import { ConfidentialContext } from '../Context/Context';
 import ApiClient from '../CommonMethods/APIHandle';
-import { Link } from 'react-router-dom';
+
+
+import { useHistory } from "react-router-dom";
 
 function CreateGroup() {
-    const {value, setValue} = useContext(ConfidentialContext);
+    const { value, setValue } = useContext(ConfidentialContext);
     const [groups, setGroups] = useState(null);
+
+    let history = useHistory()
 
     console.log(value, '..value')
 
-    useEffect(()=>{
+    useEffect(() => {
 
         const apiUrl = `http://localhost:3090/group/${value.userId}`;
 
-        ApiClient.sendGetGroup(apiUrl, (res)=>{
-            if(res.data){
+        ApiClient.sendGetGroup(apiUrl, (res) => {
+            if (res.data) {
                 console.log('Create-Groups', res)
                 setGroups(res.data.groupsOwned)
             }
-            else{
+            else {
                 console.log(res)
-            }	
-        })  
-    },[])
+            }
+        })
+    }, [])
 
-    useEffect(()=> {
+    useEffect(() => {
         console.log(groups, 'groups')
     }, [groups])
 
+    const showGroupDetail = (id) => {
+        history.push(`/organize/${id}`)
+    }
     return (
         <div className='join-cont'>
-            <div className='join-upper' style={{display: 'flex', justifyContent:'center'}}>
-                    <p className='join-title' style={{textAlign:'center'}} >All Created Groups</p>                
+            <div className='join-upper' style={{ display: 'flex', justifyContent: 'center' }}>
+                <p className='join-title' style={{ textAlign: 'center' }} >All Created Groups</p>
             </div>
-            <div className='grid' style={{display: 'grid', gridTemplateColumns: '1fr 1fr'}}>
-                {groups ? groups.map((user)=>(
-                    <div className='join-right-cont' >
-                    <div className='group'>
-                        <div className='group-pic' >
-                            <img src='/Event.png' alt='img'></img>
+            <div className='grid' style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+                {groups ? groups.map((group) => (
+                    <div onClick={() => showGroupDetail(group._id)} className='join-right-cont' >
+                        <div className='group'>
+                            <div className='group-pic' >
+                                <img src='/Event.png' alt='img'></img>
+                            </div>
+                            <div className='join-org-btn'>
+                                <button className='join-btn'>Join</button>
+                                <button className='org-login'>Organizer Login</button>
+                            </div>
                         </div>
-                        <div className='join-org-btn'>
-                            <button className='join-btn'>Join</button>
-                            <button className='org-login'>Organizer Login</button>
+                        <div className='event'>
+                            <p>{group.groupName}</p>
                         </div>
                     </div>
-                    <div className='event'>
-                        <p>Group 1</p>
-                    </div>
-                     </div>
                 ))
-                :
-                <div>No Groups Created</div>
-            
-            }
-                
+                    :
+                    <div>No Groups Created</div>
+
+                }
+
             </div>
         </div>
     )
